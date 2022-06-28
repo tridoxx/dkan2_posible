22e7ff0fbab9c76c20b2bc57dfcc9b80f391dee1
Holger Kreis <holger@markaspot.org>
2022-06-28 20:17:48 +0200

    Add composer install on container start

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,20 +3,28 @@
 echo "Client ID is $CLIENTID";
 URL=https://github.com/markaspot/$CLIENTID/archive/refs/heads/master.zip
 echo "Installing from $URL";
-# exit;
 
+
+composer install \
+    --ignore-platform-reqs \
+    --no-interaction \
+    --no-dev \
+    --prefer-dist;
 ## Override frontend
 
 # echo "Override Frontend with CGN UI"
-# rm -rf src/frontend
+rm -rf src/frontend
 
 # Get latest customizations from client's repo
 # wget --no-check-certificate https://github.com/markaspot/data-catalog-app/archive/refs/heads/cgn.zip
 # unzip cgn.zip -d /tmp/ && rm cgn.zip
 # cp -r /tmp/data-catalog-app-cgn src/frontend && rm -rf /tmp/data-catalog-app-cgn
 
-#git clone https://github.com/GetDKAN/data-catalog-react.git src/frontend
-# cd src/frontend && npm run build
+git clone https://github.com/GetDKAN/data-catalog-react.git src/frontend
+cd src/frontend/
+npm install node-sass@npm:sass
+npm install && npm run build
+
 
 
 
@@ -24,6 +32,7 @@ drush site:install standard --site-name "DKAN" -y
 drush en dkan dkan config_update_ui -y
 drush config-set system.performance css.preprocess 0 -y
 drush config-set system.performance js.preprocess 0 -y
+
 mkdir -p sites/default/files/uploaded_resources
 mkdir -p sites/default/files/resources
 chmod -R 777 sites/default/files
@@ -41,16 +50,10 @@ drush  queue:run datastore_import
 drush  dkan:metastore-search:rebuild-tracker
 drush  sapi-i
 
-
- # cd src/frontend/
- # npm install
-
  # Get latest customizations from client's repo
  $URL = https://github.com/markaspot/$CLIENTID/archive/refs/heads/master.zip
  wget --no-check-certificate https://github.com/markaspot/$CLIENTID/archive/refs/heads/master.zip
  unzip master.zip -d /tmp/ && rm master.zip
  mkdir -p docroot/schema && cp -r /tmp/$CLIENTID-master/schema/collections docroot/schema/
- rm -r /tmp/$CLIENTID-master
- echo "Installation done"
-~
-~
+ rm -rf /tmp/$CLIENTID-master
+ echo "Installation done";
